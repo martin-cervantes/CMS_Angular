38b9c7973002c13675bc38da961ca4c1bbb06eb9
Martin Cervantes <cervantes.martine@gmail.com>
2021-08-26 16:41:15 -0600

    Add display modal function to create user button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'CMS Angular';
+  elementModal: any;
+
+  displayModal() {
+    this.elementModal = document.getElementById("modal") as HTMLElement
+    this.elementModal.style.display = "block"
+  }
 }
